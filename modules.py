5a7ee8776acbfa89ab768d1eefaee41f7a43a100
erhelito <erhelito@yahoo.fr>
2021-07-22 12:44:06 +0200

    finishing modules

--- modules.py
+++ modules.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3.8
 # -*-coding:Utf-8 -*
 
-from os import remove
+from os import remove, path, mkdir
+from requests import get
 
 def url_recovering(list) :
     url_list = open("url_list.txt", "w")
@@ -16,6 +17,7 @@ def url_recovering(list) :
 def url_sorting() :
     j = ""
     useless_url = "https://www.oiseaux.net/oiseaux/\n"
+    sorted_urls = []
 
     file = open("url_list.txt", "r")
     urls = file.readlines()
@@ -28,7 +30,51 @@ def url_sorting() :
 
         elif i == j:
             urls.remove(i)
-            
+
         j = i
 
-    return urls
+    for i in urls :
+        sorted_urls.append(i[0:-1])
+
+    return sorted_urls
+
+def information_file_and_image(browser, article_url) :
+    name = browser.find_element_by_class_name("titre")
+    name = name.text
+
+    print(name)
+
+    order = browser.find_element_by_class_name("order")
+    order = order.text
+
+    description = browser.find_element_by_xpath('//*[@id="description-esp"]/div/p')
+    description = description.text
+
+    data = browser.find_elements_by_class_name("on_bio_titre")
+    data = data[2]
+    data = data.text
+
+    image_url = browser.find_element_by_class_name("on_img_id")
+    image_url = image_url.get_attribute("src")
+    image_url = get(image_url)
+
+    if path.exists(order) is False :
+        mkdir(order)
+
+    image = open(f"{order}/{name}.png", "wb")
+    image.write(image_url.content)
+    image.close()
+
+    description_file = open(f"{order}/{name}.txt", "w")
+    description_file.write(
+        f"""Nom : {name}
+Ordre : {order}
+
+{data}
+
+Description : {description}
+
+URL de l'article pour plus d'informations : {article_url}"""
+    )
+
+    print("Done")
